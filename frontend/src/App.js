77257d8ae21277fe7c1802fe5a1358eb7fcd2577
BY-ee <gjwjqgh3@naver.com>
2024-11-29 15:38:10 +0900

    add(main): 페이지 렌더링 시 fade-in 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DetailItem from "./pages/DetailItem/DetailItem";
-import Home from "./pages/main/Home";
+import Home from './pages/main/Home';
+import Animsition from './components/Animsition';
 
 const AppRoutes = () => (
-
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-{/*
-            <Route path="/" element={<DetailItem/>} />
-*/}
-            {/* 추가 라우트 */}
-        </Routes>
-    </Router>
-
+  <Router>
+    <Animsition>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Animsition>
+  </Router>
 );
 
 export default AppRoutes;
--- frontend/src/hooks/useFixedHeader.js
+++ frontend/src/hooks/useFixedHeader.js
@@ -1,18 +1,18 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from 'react';
 
 function useFixedHeader() {
-    const [isAtTop, setIsAtTop] = useState(true);
+  const [isAtTop, setIsAtTop] = useState(true);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsAtTop(window.scrollY === 0);
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY === 0);
+    };
 
-        window.addEventListener("scroll",handleScroll);
-        return () => window.removeEventListener("scroll",handleScroll);
-    }, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
-    return isAtTop;
+  return isAtTop;
 }
 
 export default useFixedHeader;
