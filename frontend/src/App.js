6def83de5c71379a93d3948799675237f0a5aefb
Sanghyeok Lee <hyunu29@gmail.com>
2024-11-29 17:32:37 +0900

    로그인 및 회원가입 폼 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DetailItem from "./pages/DetailItem/DetailItem";
-import Home from "./pages/main/Home";
+import DetailItem from './pages/DetailItem/DetailItem';
+import Home from './pages/main/Home';
+import Login from './pages/Auth/Login';
+import SingUp from './pages/Auth/SignUp';
 
 const AppRoutes = () => (
-
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-{/*
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SingUp />} />
+      {/*
             <Route path="/" element={<DetailItem/>} />
 */}
-            {/* 추가 라우트 */}
-        </Routes>
-    </Router>
-
+      {/* 추가 라우트 */}
+    </Routes>
+  </Router>
 );
 
 export default AppRoutes;
