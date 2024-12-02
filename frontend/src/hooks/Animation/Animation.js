3aa5067a61db0e2b509b09e96295ab9201fab947
NAMEJM-sys <dlwjdals0803@gmail.com>
2024-12-02 22:15:06 +0900

    ㅁㄴㅇ

--- frontend/src/hooks/Animation/Animation.js
+++ frontend/src/hooks/Animation/Animation.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../../assets/vendor/animsition/css/animsition.css';
 import { useNavigate } from 'react-router-dom';
 
-export default function Animation({ children }) {
+export default function Animsition({ children }) {
     const [isVisible, setIsVisible] = useState(false); // fade-in 상태
     const [fadeOut, setFadeOut] = useState(false); // fade-out 상태
     const navigate = useNavigate();
@@ -13,6 +13,13 @@ export default function Animation({ children }) {
         return () => clearTimeout(timer);
     }, []);
 
+    // const handleLinkClick = (url) => {
+    //   setFadeOut(true); // fade-out 시작
+    //   setTimeout(() => {
+    //     navigate(url); // fade-out 완료 후 페이지 이동
+    //   }, 800); // fade-out 애니메이션 시간과 일치
+    // };
+
     return (
         <div
             className={`animsition ${fadeOut ? 'fade-out' : isVisible ? 'fade-in' : ''}`}
@@ -21,10 +28,3 @@ export default function Animation({ children }) {
         </div>
     );
 }
-
-// const handleLinkClick = (url) => {
-//   setFadeOut(true); // fade-out 시작
-//   setTimeout(() => {
-//     navigate(url); // fade-out 완료 후 페이지 이동
-//   }, 800); // fade-out 애니메이션 시간과 일치
-// };
