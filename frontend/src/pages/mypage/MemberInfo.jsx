616526c8e20a0ce1346224c116b437677e3d1f39
codeychoi <codeychoi@gmail.com>
2024-12-04 15:03:31 +0900

    마이페이지 - 배송지 중간저장

--- frontend/src/pages/mypage/MemberInfo.jsx
+++ frontend/src/pages/mypage/MemberInfo.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import MyPageHeader from '../../components/myPage/MyPageHeader';
 
 const MemberInfo = () => {
-  const [address, setAddress] = useState([]);
+  const [representativeAddress, setRepresentativeAddress] = useState([]);
+  const [generalAddresses, setGeneralAddresses] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,7 +14,11 @@ const MemberInfo = () => {
 
         const address = await addressResponse.json();
 
-        setAddress(address);
+        const representative = address.find((addr) => addr.isRepresent === 1); // 대표 주소
+        const general = address.filter((addr) => addr.isRepresent !== 1); // 일반 주소
+
+        setRepresentativeAddress(representative);
+        setGeneralAddresses(general);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -26,23 +31,43 @@ const MemberInfo = () => {
       <div>
         <MyPageHeader title="회원정보" description="회원정보 등록 및 수정" />
       </div>
-      <div className="inquirement-label">회원정보</div>
+      <div className="mypage-label1">회원정보</div>
       <div className="rounded-box"></div>
 
-      <div className="inquirement-label">배송지 관리</div>
+      <div className="mypage-label1">배송지 관리</div>
+
       <div className="delivery-rounded-box">
-        {address.map((address) => (
-          <div key={address.id} className="address-block">
-            <div className="represent-address">
-              {address.isRepresent ? '대표' : '일반'}
+        {!representativeAddress && generalAddresses.length === 0 ? (
+          <div className="mypage-label2">등록된 배송지가 없습니다.</div>
+        ) : (
+          <>
+            {/* 대표 주소 */}
+            <div className="address-block">
+              <div className="represent-address">대표주소지</div>
+              <div className="other-address">
+                {representativeAddress.address}
+              </div>
+              <div className="address-buttons">
+                <button>삭제</button>
+              </div>
             </div>
-            <div className="other-address">{address.address}</div>
-            <div className="address-buttons">
-              <button>대표주소지 등록</button>
-              <button>삭제</button>
-            </div>
-          </div>
-        ))}
+
+            {/* 일반 주소 */}
+            {generalAddresses.map((address) => (
+              <div key={address.id} className="address-block">
+                <div className="represent-address">일반</div>
+                <div className="other-address">{address.address}</div>
+                <div className="address-buttons">
+                  <button>대표주소지 등록</button>
+                  <button>삭제</button>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+        <div className="address-buttons">
+          <button className="delivery-button">배송지 등록하기</button>
+        </div>
       </div>
     </div>
   );
